3071108a64d7b657f1197edc88163fe1c5e7c6f5
varshadhanotiya <varshadhanotiya7026@gmail.com>
2023-08-09 17:33:49 +0530

    Book List App

---
+++ BookListApp/app.js
@@ -0,0 +1,57 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+    }
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
+    static displayBooks() {
+      const StoredBooks = [
+        {
+            title: 'Book One',
+            author: 'John Doe',
+            isbn: '323232'
+        },
+        {
+            title: 'Book Two',
+            author: 'Jone Doe',
+            isbn: '121212'
+        }
+      ];
+
+      const books = StoredBooks;
+  
+      books.forEach((book) => UI.addBookToList(book));
+    }
+  
+    static addBookToList(book) {
+      const list = document.querySelector('#book-list');
+  
+      const row = document.createElement('tr');
+  
+      row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
+  
+      list.appendChild(row);
+    }
+  }
+  
+  // Store Class: Handles Storage
+  
+  // Event: Display Books
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+  
+  // Event: Add a Book
+  
+  // Event: Remove a Book
+  
+    // Remove book from store
+  
+    // Show success message
