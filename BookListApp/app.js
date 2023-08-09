53d4b696dc71a58d89eba32bc9bdbcb289118761
varshadhanotiya <varshadhanotiya7026@gmail.com>
2023-08-09 18:04:22 +0530

    add book and display details in console

--- BookListApp/app.js
+++ BookListApp/app.js
@@ -49,6 +49,21 @@ class Book {
   document.addEventListener('DOMContentLoaded', UI.displayBooks);
   
   // Event: Add a Book
+
+  document.querySelector('#book-form').addEventListener('submit' , (e)=>{
+      //prevent actual submit
+      e.preventDefault();
+      
+      //get form values
+      const title = document.querySelector('#title').value;
+      const author = document.querySelector('#author').value;
+      const isbn = document.querySelector('#isbn').value;
+
+      //instantiate book
+      const book = new Book(title,author,isbn);
+
+      console.log(book);
+  });
   
   // Event: Remove a Book
   
